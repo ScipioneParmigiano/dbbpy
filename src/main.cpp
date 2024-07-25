a4d409ee35647813933ecde30096b9709707aebd
ScipioneParmigiano <pcs.parmigiano@gmail.com>
2024-07-25 10:26:09 +0200

    remove py stuff from option_clenaner

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "optimizer.cpp"
 #include "option_cleaner.cpp"
 #include "option_cleaner.hpp"
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 
 PYBIND11_MODULE(bindings, m) {
     m.doc() = ""; 
@@ -9,7 +11,6 @@ PYBIND11_MODULE(bindings, m) {
     // m.def("otm_payoff", &otm_payoff, "Compute out-of-the-money payoff");
     // m.def("omegaLMask", &omegaLMask, "Compute omegaL mask");
     m.def("performOptimization", &performOptimization, "Perform optimization");
-
     
     m.def("getFeasibleOptionFlags", &getFeasibleOptionFlags, "");
 
--- src/option_cleaner.hpp
+++ src/option_cleaner.hpp
@@ -1,21 +1,21 @@
-/*
- * Copyright 2021 <copyright holder> <email>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// /*
+//  * Copyright 2021 <copyright holder> <email>
+//  *
+//  * Licensed under the Apache License, Version 2.0 (the "License");
+//  * you may not use this file except in compliance with the License.
+//  * You may obtain a copy of the License at
+//  *
+//  *     http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  * Unless required by applicable law or agreed to in writing, software
+//  * distributed under the License is distributed on an "AS IS" BASIS,
+//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  * See the License for the specific language governing permissions and
+//  * limitations under the License.
+//  */
 
 #ifndef OPTION_CLEANER_HPP
-#define OPTION_CLEANER
+#define OPTION_CLEANER_HPP
 
 #include <cmath>
 #include <iostream>
@@ -43,26 +43,13 @@ typedef mosek::fusion::Expr M_Expr;
 
 typedef mosek::fusion::Model::t M_Model; 
 
-// template<typename T, typename... Args>
-//     std::unique_ptr<T> make_unique(Args&&... args) {
-//     return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-// }
-
-// inline double  otm_payoff(double state, double strike, bool pFlag)  {
-//     if(pFlag){
-//         return(std::max(strike - state,0.0));
-//     }
-//     return(std::max(state - strike,0.0));
-// }
-
-
 // Function to compute feasible option flags
 Eigen::Matrix<bool, Eigen::Dynamic, 1> getFeasibleOptionFlags(
-    py::array_t<double> sp_py,
-    py::array_t<double> bid_py,
-    py::array_t<double> ask_py,
-    py::array_t<double> strike_py,
-    py::array_t<bool> pFlag_py,
+const Eigen::VectorXd& sp,
+    const Eigen::VectorXd& bid,
+    const Eigen::VectorXd& ask,
+    const Eigen::VectorXd& strike,
+    const Eigen::Matrix<bool, Eigen::Dynamic, 1>& pFlag,
     double spotsP, 
     double spbid,
     double spask
@@ -70,35 +57,33 @@ Eigen::Matrix<bool, Eigen::Dynamic, 1> getFeasibleOptionFlags(
 
 // Function to compute mid price Q
 Eigen::VectorXd getMidPriceQ(
-    py::array_t<double> sp_py,
-    py::array_t<double> bid_py,
-    py::array_t<double> ask_py,
-    py::array_t<double> strike_py,
-    py::array_t<bool> pFlag_py,
+    const Eigen::VectorXd& sp,
+    const Eigen::VectorXd& bid,
+    const Eigen::VectorXd& ask,
+    const Eigen::VectorXd& strike,
+    const Eigen::Matrix<bool, Eigen::Dynamic, 1>& pFlag,
     double spotsP, 
     double spbid,
-    double spask
+    double spask    
     );
 
 // Function to compute mid price Q with regularization
 Eigen::VectorXd getMidPriceQReg(
-    py::array_t<double> sp_py,
-    py::array_t<double> bid_py,
-    py::array_t<double> ask_py,
-    py::array_t<double> strike_py,
-    py::array_t<bool> pFlag_py,
+    const Eigen::VectorXd& sp,
+    const Eigen::VectorXd& bid,
+    const Eigen::VectorXd& ask,
+    const Eigen::VectorXd& strike,
+    const Eigen::Matrix<bool, Eigen::Dynamic, 1>& pFlag,
     double spotsP, 
     double spbid,
-    double spask
-    );
+    double spask);
 
-// Function to compute Q regression
+// Function to compute Q with regularization
 Eigen::VectorXd getQReg(
-    py::array_t<double> sp_py,
-    py::array_t<double> bid_py,
-    py::array_t<double> ask_py,
-    py::array_t<double> strike_py,
-    py::array_t<bool> pFlag_py,
+    const Eigen::VectorXd& bid,
+    const Eigen::VectorXd& ask,
+    const Eigen::VectorXd& strike,
+    const Eigen::Matrix<bool, Eigen::Dynamic, 1>& pFlag,
     double spotsP, 
     double spbid,
     double spask
